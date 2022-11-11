b8acb2d92ba29b3befb7c20bbeaafa116f4d9860
eskilDigernes <112086034+eskilDigernes@users.noreply.github.com>
2022-11-11 17:28:49 +0100

    Update ForwardKinematics.m

--- ForwardKinematics.m
+++ ForwardKinematics.m
@@ -1,15 +1,21 @@
- syms q1 q2 q3 q4
- L1 = 200; L2 = 250; L3 = 450; L4 = 100; %All values [mm]
+syms q1 q2 q3 q4
 
+%Defining link-lengths. All values [mm]
+L1 = 200; L2 = 250; L3 = 450; L4 = 100; 
+
+%Connecting the links and with our angles theta and alfa
+%             |th|d|a|alf  
 L(1) = Link ( [0 -L1 0 pi/2] );
 L(2) = Link ( [0 0 L2 0] );
 L(3) = Link ( [0 0 L3 0] );
 L(4) = Link ( [0 0 L4 0] );
 
- RobotArm = SerialLink(L)
- RobotArm.name = 'Hexacopter Arm';
- RobotArm.teach
- 
+% Creating an object of the links 
+RobotArm = SerialLink(L)
+RobotArm.name = 'Hexacopter Arm';
+
+
 import ETS3.*
-E = Rz('q1')*Tz(L1)*Ry('q2')*Tz(L2)*Ry('q3')*Tz(L3)*Ry('q4')*Tz(L4);
-%E.teach
+E = Rz('q1')*Tz(-L1)*Ry('q2')*Tx(L2)*Ry('q3')*Tx(L3)*Ry('q4')*Tx(L4);
+E.teach
+
