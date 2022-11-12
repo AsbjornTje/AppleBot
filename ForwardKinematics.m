b69713ae62b869f29f5a979db5df4c7ad90b905c
eskilDigernes <112086034+eskilDigernes@users.noreply.github.com>
2022-11-12 16:27:32 +0100

    Update ForwardKinematics.m

--- ForwardKinematics.m
+++ ForwardKinematics.m
@@ -1,10 +1,13 @@
 syms q1 q2 q3 q4
 
-%Defining link-lengths. All values [mm]
-L1 = 200; L2 = 250; L3 = 450; L4 = 100; 
+%Defining link-lengths. All values [m]
+L1 = .250; L2 = .45; L3 = .45; L4 = .25;
+
+%Creating zero angle variable
+qz = ([0 0 0 0]);
 
 %Connecting the links with the DH-parameters: 
-% | theta | d | a | alfa |  
+%| theta | d | a | alfa |  
 L(1) = Link ( [0 -L1 0 pi/2] );
 L(2) = Link ( [0 0 L2 0] );
 L(3) = Link ( [0 0 L3 0] );
@@ -14,17 +17,27 @@ L(4) = Link ( [0 0 L4 0] );
 robotArm = SerialLink(L);
 robotArm.name = 'Hexacopter Arm';
 
-%Creating zero angle variable
-qz = ([0 0 0 0]);
-
 %Attampt to recreate our arm
 import ETS3.*
 attamptArm = Rz('q1')*Tz(-L1)*Ry('q2')*Tx(L2)*Ry('q3')*Tx(L3)*Ry('q4')*Tx(L4);
+
+%finding T04. Uncomment 'syms' if you symbloic output  
+%syms Q1 Q2 Q3 Q4       
+Q1=0; Q2=0; Q3=0; Q4=0;
+T01 = trchain ('Rz(Q1)*Tz(-L1)', [Q1]);
+T12 = trchain ('Ry(Q2)*Tx(L2)', [Q2]);
+T23 = trchain ('Ry(Q3)*Tx(L3)', [Q3]);
+T34 = trchain ('Ry(Q4)*Tx(L4)', [Q4]);
+
+%Checking if T04 is the same as kinematics
+T04 = T01*T12*T23*T34
+kineAttampt = attamptArm.fkine(qz)
 
 %Plotting both robot arms
 figure('Name','DH-Parameters')
 robotArm.plot(qz)
 figure('Name','Attampt arm')
-attamptArm.plot(qz)
+
+
 
 
